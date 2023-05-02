b86f46209354a3623318d7274bb237936f2d4484
Priya Kasimbeg <kasimbeg@google.com>
2023-05-02 00:34:48 +0000

    debugging

--- algorithmic_efficiency/debug_utils.py
+++ algorithmic_efficiency/debug_utils.py
@@ -3,7 +3,7 @@ from absl import logging
 import datetime
 
 now = datetime.datetime.now().strftime('-%Y-%m-%d-%H-%M-%S')
-DEBUG_FILE = f'/logs/memory_log{now}.txt'
+DEBUG_FILE = f'/home/kasimbeg/logs/memory_log{now}.txt'
 def log_mem_usage(event):
     message = f"{event}: RAM USED (GB) {psutil.virtual_memory()[3]/1000000000}\n"
     logging.info(message)
